9b26eb2803312dcf05a60be0e582bea22d1899b1
Hyouhyan <hyouhyan@hyouhyan.com>
2025-11-14 23:01:49 +0900

    fix: スクリプトの呼び出しを修正

--- init.sh
+++ init.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+BASE_URL="https://raw.githubusercontent.com/hyouhyan/server-init/refs/heads/main/"
+CURL_COMMAND="curl -fsSL"
+
 . /etc/os-release
 
 if [ "$ID" = "ubuntu" ]; then
-    sh init-ubuntu-server.sh
+    $CURL_COMMAND "${BASE_URL}init-ubuntu-server.sh" | sh
 elif [ "$ID" = "debian" ]; then
-    sh init-debian-server.sh
+    $CURL_COMMAND "${BASE_URL}init-debian-server.sh" | sh
 else
     echo "unsupported: $ID"
 fi
