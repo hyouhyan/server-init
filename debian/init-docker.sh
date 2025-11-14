fb4e39f7d48b8216ccde38190071563db4bed872
hyouhyan <hyouhyan@hyouhyan.com>
2025-11-15 01:32:32 +0900

    feat: dockerのコード分離

---
+++ debian/init-docker.sh
@@ -0,0 +1,23 @@
+# Dockerのインストール
+## Add Docker's official GPG key:
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+## Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+
+## Dockerのインストール
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+## sudoなしでDockerを動かせるようにする
+sudo usermod -aG docker $(echo $USER)
--- ubuntu/init-ubuntu-server.sh
+++ ubuntu/init-ubuntu-server.sh
@@ -9,25 +9,7 @@ sudo apt update
 
 
 
-# Dockerのインストール
-## Add Docker's official GPG key:
-sudo apt install -y ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-## Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-
-## Dockerのインストール
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-## sudoなしでDockerを動かせるようにする
-sudo usermod -aG docker $(echo $USER)
 
 
 # VPN用設定
