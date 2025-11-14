f6cc8ddfb3a59ed2598089a5c1e5c333e4acf133
hyouhyan <hyouhyan@hyouhyan.com>
2025-11-15 01:36:33 +0900

    feat: 設定を分離していい感じに

--- init.sh
+++ init.sh
@@ -1,14 +1,53 @@
 #!/bin/bash
 
+# 下準備
+
+## いろんな定数
 BASE_URL="https://raw.githubusercontent.com/hyouhyan/server-init/refs/heads/main/"
 CURL_COMMAND="curl -fsSL"
 
+## OS情報取得
 . /etc/os-release
 
+## パッケージアップデート
+sudo apt update
+
+
+# 日本語化
 if [ "$ID" = "ubuntu" ]; then
-    $CURL_COMMAND "${BASE_URL}init-ubuntu-server.sh" | sh
+    $CURL_COMMAND "${BASE_URL}/ubuntu/init-jp.sh" | sh
 elif [ "$ID" = "debian" ]; then
-    $CURL_COMMAND "${BASE_URL}init-debian-server.sh" | sh
+    $CURL_COMMAND "${BASE_URL}/ubuntu/init-jp.sh" | sh
+else
+    echo "unsupported: $ID"
+fi
+
+
+# Dockerのインストール
+if [ "$ID" = "ubuntu" ]; then
+    $CURL_COMMAND "${BASE_URL}/ubuntu/init-docker.sh" | sh
+elif [ "$ID" = "debian" ]; then
+    $CURL_COMMAND "${BASE_URL}/ubuntu/init-docker.sh" | sh
+else
+    echo "unsupported: $ID"
+fi
+
+
+# VPN用設定
+if [ "$ID" = "ubuntu" ]; then
+    $CURL_COMMAND "${BASE_URL}/ubuntu/init-vpn.sh" | sh
+elif [ "$ID" = "debian" ]; then
+    $CURL_COMMAND "${BASE_URL}/ubuntu/init-vpn.sh" | sh
+else
+    echo "unsupported: $ID"
+fi
+
+
+# サーバ保守関係
+if [ "$ID" = "ubuntu" ]; then
+    $CURL_COMMAND "${BASE_URL}/ubuntu/init-autoupdate.sh" | sh
+elif [ "$ID" = "debian" ]; then
+    $CURL_COMMAND "${BASE_URL}/ubuntu/init-autoupdate.sh" | sh
 else
     echo "unsupported: $ID"
 fi
