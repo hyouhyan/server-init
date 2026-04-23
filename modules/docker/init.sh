79e4732775754e133bfc419653df2ade91c99038
hyouhyan <hyouhyan@hyouhyan.com>
2026-04-23 14:11:56 +0900

    feat: docker用スクリプト

---
+++ modules/docker/init.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Dockerのインストール・設定モジュール
+
+## Uninstall old versions
+sudo apt remove $(dpkg --get-selections docker.io docker-compose docker-doc podman-docker containerd runc | cut -f1) 2>/dev/null || true
+
+case "$ID" in
+    ubuntu)
+        # Add Docker's official GPG key
+        sudo apt-get -q install -y ca-certificates curl
+        sudo install -m 0755 -d /etc/apt/keyrings
+        sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+        sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+        # Add the repository to Apt sources
+        echo \
+          "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+          $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+          sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        sudo apt-get update
+        ;;
+    debian)
+        # Add Docker's official GPG key
+        sudo apt-get -q install -y ca-certificates curl
+        sudo install -m 0755 -d /etc/apt/keyrings
+        sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+        sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+        # Add the repository to Apt sources
+        sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+        sudo apt update
+        ;;
+    *)
+        echo "Unsupported OS: $ID"
+        return 1
+        ;;
+esac
+
+## Install Docker
+sudo apt-get -q install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+## Allow running Docker without sudo
+sudo usermod -aG docker "$(whoami)"
