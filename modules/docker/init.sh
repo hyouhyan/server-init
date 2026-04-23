edd97bb1590a32d7138dd91eb38c3086c4242dc2
hyouhyan <hyouhyan@hyouhyan.com>
2026-04-23 14:26:53 +0900

    feat: dockerのソースを公式ドキュメントの様式に更新

--- modules/docker/init.sh
+++ modules/docker/init.sh
@@ -14,10 +14,15 @@ case "$ID" in
         sudo chmod a+r /etc/apt/keyrings/docker.asc
 
         # Add the repository to Apt sources
-        echo \
-          "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-          $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-          sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
         sudo apt-get update
         ;;
     debian)
@@ -33,6 +38,7 @@ Types: deb
 URIs: https://download.docker.com/linux/debian
 Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
 Components: stable
+Architectures: $(dpkg --print-architecture)
 Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 
